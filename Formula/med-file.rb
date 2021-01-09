fab7e01d554fe838e2c65072e2a1a0d3e777778e
Andreas Sandberg <andreas@sandberg.uk>
2021-01-09 22:57:42 +0000

    med-file: Clean up formula
    Signed-off-by: Andreas Sandberg <andreas@sandberg.uk>

--- Formula/med-file.rb
+++ Formula/med-file.rb
@@ -4,31 +4,33 @@ class MedFile < Formula
   url "http://files.salome-platform.org/Salome/other/med-4.0.0.tar.gz"
   sha256 "a474e90b5882ce69c5e9f66f6359c53b8b73eb448c5f631fa96e8cd2c14df004"
 
+  bottle do
+    root_url "https://dl.bintray.com/vejmarie/freecad"
+    sha256 cellar: :any, big_sur:  "21dc7b948d4bf3e022690bd075ed9f6e623c7d08088178f60a4f9f9acc70367c"
+    sha256 cellar: :any, cataline: "d66199bb1cbd71baf8f17bbef258fe64f02fe6f7cfc21427555f3c5b31297e1d"
+  end
+
   depends_on "cmake" => :build
   depends_on "gcc" => :build   # for gfortan
   depends_on "freecad/freecad/swig@4.0.2" => :build
   depends_on "hdf5@1.10"
   depends_on "freecad/freecad/python3.9"
 
-  bottle do
-    root_url "https://dl.bintray.com/vejmarie/freecad"
-    cellar :any
-    sha256 "21dc7b948d4bf3e022690bd075ed9f6e623c7d08088178f60a4f9f9acc70367c" => :big_sur
-    sha256 "d66199bb1cbd71baf8f17bbef258fe64f02fe6f7cfc21427555f3c5b31297e1d" => :catalina
-  end
-
   def install
-
     python_prefix=`#{Formula["freecad/freecad/python3.9"].opt_bin}/python3-config --prefix`.chomp
     python_include=Dir["#{python_prefix}/include/*"].first
 
-    #ENV.cxx11
-    system "cmake", ".", "-DMEDFILE_BUILD_PYTHON=ON",
-                         "-DMEDFILE_BUILD_TESTS=OFF",
-                         "-DMEDFILE_INSTALL_DOC=OFF",
-                         "-DPYTHON_INCLUDE_DIR=#{python_include}",
-                         *std_cmake_args
-    system "make", "install"
+    args = std_cmake_args + %W[
+      -DMEDFILE_BUILD_PYTHON=ON
+      -DMEDFILE_BUILD_TESTS=OFF
+      -DMEDFILE_INSTALL_DOC=OFF
+      -DPYTHON_INCLUDE_DIR=#{python_include}
+    ]
+
+    mkdir "build" do
+      system "cmake", *args, ".."
+      system "make", "install"
+    end
   end
 
   test do
@@ -46,4 +48,3 @@ class MedFile < Formula
     system "./test"
   end
 end
-
